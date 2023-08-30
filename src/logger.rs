e61e2e62cc9ce46f6d93e5d5ae0bddc91df59155
Jean NdL <jean.nicolasdelamballerie@gmail.com>
2023-08-30 09:36:49 +0200

    logger

---
+++ src/logger.rs
@@ -0,0 +1,12 @@
+// use terminal_size::{terminal_size, Height, Width};
+// use tabled::{Tabled, Table};
+// pub fn log( ) {
+//
+//     let displayedElements;
+//     let size = terminal_size();
+//     let Some((Width(w), Height(h))) = size else {panic!() };
+//     todo!()
+//
+// }
+//
+// fn table()
