7825f073f91fc72d01267be9da16ff73222eb719
Jean NdL <jean.nicolasdelamballerie@gmail.com>
2023-09-01 03:57:50 +0200

    scripts updated, dash/rush base commands

--- scripts/dash.ps1
+++ scripts/dash.ps1
@@ -1,8 +1,24 @@
 $path = $HOME + "\code\projects\my-cli\target\debug\my-cli.exe"
-function Invoke-Rust-mycli {
+function Invoke-Rust-dash {
     # TODO : handle stdout & stderr differently instead of crashing
     Write-Host moving to "$args";
     $output = cmd /c $path move "$args" 2`>`&1
     Set-Location "$output"
 }
-Set-Alias dash Invoke-Rust-mycli
+
+
+# DEV
+#
+#
+
+function Invoke-Rust-cli {
+    # TODO : handle stdout & stderr differently instead of crashing
+    Write-Host "Running debug version.";
+   cmd /c $path $args;
+
+}
+
+#
+#
+Set-Alias dash Invoke-Rust-dash
+Set-Alias rush Invoke-Rust-cli
